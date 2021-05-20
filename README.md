9d61d5d3f5629c4eb7e4517ba8d996bdc9130f82
7e12 <trieunvt7@gmail.com>
2021-05-20 12:57:45 +0700

    Add a support feature for executing on Mac

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 A messenger example based on a named pipe (FIFO)
 ## Execute
 ```$bash
-$ sh setup.sh main1.c main2.c
+$ sh setup.sh [options] main1.c main2.c
+OPTIONS:
+    -l              Execute on Linux
+    -m              Execute on Mac
 ```
--- setup.sh
+++ setup.sh
@@ -1,12 +1,31 @@
 #!/bin/sh
 
-file1=$1
-file2=$2
+while getopts lm flags
+do
+    case "${flags}" in
+        l) os="linux";;
+        m) os="mac";;
+    esac
+done
+
+file1=$2
+file2=$3
 
 cc $file1 -o ${file1%.*}.out
 cc $file2 -o ${file2%.*}.out
 
-gnome-terminal --window --title=${file1%.*} -- bash -c "./${file1%.*}.out"
-gnome-terminal --window --title=${file2%.*} -- bash -c "./${file2%.*}.out"
+if [ $os = "linux" ]
+then
+    gnome-terminal --window --title=${file1%.*} -- bash -c "./${file1%.*}.out"
+    gnome-terminal --window --title=${file2%.*} -- bash -c "./${file2%.*}.out"
+elif [ $os = "mac" ]
+then
+    directory=$(pwd)
+    osascript -e 'tell app "Terminal" to do script "'$directory/${file1%.*}.out'"'
+    osascript -e 'tell app "Terminal" to do script "'$directory/${file2%.*}.out'"'
+else
+    echo "OS not found"
+    exit
+fi
 
 exit
